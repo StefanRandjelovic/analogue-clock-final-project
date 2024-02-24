69bf9d659e6c41d8457628460238cab7921db8eb
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-24 12:50:22 +0100

    Add mobility to clock dials.

--- src/components/ClockDial.jsx
+++ src/components/ClockDial.jsx
@@ -7,26 +7,41 @@ import { darkModeStore } from "@jotai/store.js";
 // STYLES
 import "@styles/clock-dial.scss";
 
-const ClockDial = () => {
+const ClockDial = ({hour, minute, seconds, day, date}) => {
   // GLOBAL STATES
   const darkMode = useAtomValue(darkModeStore);
 
   return (
     <div className={darkMode ? "mainBody dark" : "mainBody"}>
       <div className="concentric"></div>
-      <div className="hourHandWrap">
+      <div
+        className="hourHandWrap"
+        style={{
+          transform: `rotate(${hour}deg)`,
+        }}
+      >
         <div className="hourHand">
           <div className="hourH"></div>
         </div>
       </div>
 
-      <div className="minuteHandWrap">
+      <div
+        className="minuteHandWrap"
+        style={{
+          transform: `rotate(${minute}deg)`,
+        }}
+      >
         <div className="minuteHand">
           <div className="minuteH"></div>
         </div>
       </div>
 
-      <div className="secondHandWrap">
+      <div
+        className="secondHandWrap"
+        style={{
+          transform: `rotate(${seconds}deg)`,
+        }}
+      >
         <div className="secondHand">
           <div className="secondH"></div>
         </div>
