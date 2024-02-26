fbafb5839d7c04f31074b4811b4bc359c1e5dc42
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-26 18:49:57 +0100

    Start building AlarmSettings component.

--- src/components/AlarmSettings.jsx
+++ src/components/AlarmSettings.jsx
@@ -1,9 +1,30 @@
-const AlarmSettings = () => {
-    return ( <>
-    <section>
-        AlarmSettings
-    </section>
-    </> );
-}
- 
+// GLOBAL STATES
+import { darkModeStore } from "@jotai/store.js";
+
+// DEV DEPENDENCIES
+import { useAtomValue } from "jotai";
+
+const AlarmSettings = ({
+  setAlarm,
+  setInputedHours,
+  setInputedMinutes,
+  inputedMinutes,
+  inputedHours,
+  alarm,
+  alarmTime,
+  time,
+  setShowAlarmWarrning,
+}) => {
+  // GLOBAL STATE VARIABLES
+  const darkMode = useAtomValue(darkModeStore);
+
+  return (
+    <>
+      <section
+        className={darkMode ? "Alarm-settings dark" : "Alarm-settings"}
+      ></section>
+    </>
+  );
+};
+
 export default AlarmSettings;
