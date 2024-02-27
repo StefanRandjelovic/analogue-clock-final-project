4eb8922140a060551844e8810812e038f1411bde
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-27 18:29:14 +0100

    Add necessary state variables and an outline for alarm setting field.

--- src/components/AlarmSettings.jsx
+++ src/components/AlarmSettings.jsx
@@ -1,8 +1,14 @@
+//STYLES
+import "@styles/alarm-settings.scss";
+
 // GLOBAL STATES
 import { darkModeStore } from "@jotai/store.js";
 
 // DEV DEPENDENCIES
 import { useAtomValue } from "jotai";
+
+//DEV COMPONENTS
+import { useState } from "react";
 
 const AlarmSettings = ({
   setAlarm,
@@ -18,11 +24,37 @@ const AlarmSettings = ({
   // GLOBAL STATE VARIABLES
   const darkMode = useAtomValue(darkModeStore);
 
+  // STATE VARIABLES
+  const [hourInp, setHourInp] = useState("");
+  const [minuteInp, setMinuteInp] = useState("");
+  const [showHourModal, setShowHourModal] = useState(false);
+  const [showMinuteModal, setShowMinuteModal] = useState(false);
+  const [inputError, setInputError] = useState(false);
+  const [inputErrorMessage, setInputErrorMessage] = useState("");
+
   return (
     <>
-      <section
-        className={darkMode ? "Alarm-settings dark" : "Alarm-settings"}
-      ></section>
+      <section className={darkMode ? "Alarm-settings dark" : "Alarm-settings"}>
+        <h1>Alarm settings</h1>
+        <form>
+          <div className="inputs">
+            <input
+              type="text"
+              name="hourInp"
+              disabled
+              placeholder="hh"
+            />
+            <input
+              type="text"
+              name="minuteInp"
+              disabled
+              placeholder="mm"
+            />
+          </div>
+          {inputError && <p className="error">{inputErrorMessage}</p>}
+          <button type="submit">Set Alarm</button>
+        </form>
+      </section>
     </>
   );
 };
