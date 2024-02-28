8274b75e3b2cad37e50a3a49225137df558fe909
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-02-28 18:34:35 +0100

    Add alarmStore global variable.

--- src/jotai-global-state/store.js
+++ src/jotai-global-state/store.js
@@ -1,9 +1,11 @@
 // DEV DEPENDENCY
-import {atomWithStorage} from 'jotai/utils'
+import { atomWithStorage } from "jotai/utils";
 
 // GLOBAL VARIABLES
 const darkModeStore = atomWithStorage("darkMode", false);
 
 const fontStore = atomWithStorage("fontFamily", "Aldrich");
 
-export {darkModeStore, fontStore}
+const alarmStore = atomWithStorage("alarmTimes", []);
+
+export { darkModeStore, fontStore, alarmStore };
